f2ef7520a0ae1880db888a54616f717ad5147baa
MENG BAI <mb4060@columbia.edu>
2019-03-20 23:07:26 -0400

    Create Card.java

---
+++ project 4/Card.java
@@ -0,0 +1,22 @@
+
+public class Card implements Comparable<Card>{
+	
+	private int suit; // use integers 1-4 to encode the suit
+	private int value; // use integers 1013 to encode the value
+	
+	public Card(int s, int v){
+		//make a card with suit s and value v
+	}
+	
+	public int compareTo(Card c){
+		// use this method to compare cards so they 
+		// may be easily sorted
+
+	}
+	
+	public String toString(){
+		// use this method to easily print a Card object
+	}
+	// add some more methods here if needed
+
+}
